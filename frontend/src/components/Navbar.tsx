13d23db98fcdd10bec2ea507bb0f0c78dfb86410
Ethan Hu <ethanhu912@gmail.com>
2026-01-18 00:49:26 -0800

    something chaned

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center gap-3 group">
-            <img src="/HoneyKey Default Logo No Text.svg" alt="HoneyKey" className="h-10 w-10 group-hover:scale-105 transition-transform" />
+            <img src="/honeykey-logo.svg" alt="HoneyKey" className="h-10 w-10 group-hover:scale-105 transition-transform" />
             <span className="text-2xl font-bold text-[#023D50]">HoneyKey</span>
           </Link>
           
